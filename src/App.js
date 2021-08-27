30df75ecd4700bc6f8f8a239c36a15a20ae84716
PablitoLuquito <pablolucas-1@outlook.com>
2021-08-27 15:52:07 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import { useEffect, useState } from "react";
+import CheckboxInput from "./components/CheckboxInput";
+import DateInput from "./components/DateInput";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import OnlineOffline from "./components/OnlineOffline";
+import TextInput from "./components/TextInput";
+import Timer from "./components/Timer";
+import { getAgeFrom } from "./helpers/dateHelpers";
+import { getNewId } from "./services/idServices";
+
+export default function App() {
+  const [name, setName] = useState("Pablo");
+  const [birthDate, setBirthDate] = useState("1999-08-30");
+  const [showTimer, setShowTimer] = useState(false);
+  const [isOnline, setIsOnline] = useState(true);
+
+  useEffect(() => {
+    document.title = name;
+  }, [name]);
+
+  useEffect(() => {
+    function toggleOnline() {
+      setIsOnline(true);
+    }
+
+    function toggleOffline() {
+      setIsOnline(false);
+    }
+
+    window.addEventListener("online", toggleOnline);
+    window.addEventListener("offline", toggleOffline);
+
+    return () => {
+      window.removeEventListener("online", toggleOnline);
+      window.removeEventListener("offline", toggleOffline);
+    };
+  }, []);
+
+  function handleNameChange(newName) {
+    setName(newName);
+  }
+
+  function handleBirthDateChange(newBirthDate) {
+    setBirthDate(newBirthDate);
+  }
+
+  function toggleShowTimer() {
+    setShowTimer((currentShowTimer) => !currentShowTimer);
+  }
+  return (
+    <>
+      <Header>react-hello</Header>
+
+      <Main>
+        <OnlineOffline isOnline={{ isOnline }} />
+        {showTimer && (
+          <div className="text-right mt-1">
+            <Timer />
+          </div>
+        )}
+
+        <CheckboxInput
+          labelDescription="Mostrar cronômetro"
+          onCheckboxChange={toggleShowTimer}
+        />
+
+        <TextInput
+          id={getNewId()}
+          labelDescription="Digite o seu nome:"
+          inputValue={name}
+          onInputChange={handleNameChange}
+          autoFocus
+        />
+
+        <DateInput
+          id={getNewId()}
+          labelDescription="Digite a sua data de nascimento:"
+          inputValue={birthDate}
+          onInputChange={handleBirthDateChange}
+        />
+
+        <p>
+          O seu nome é {name}, com {name.length} caracteres, e você possui{" "}
+          {getAgeFrom(birthDate)} anos.
+        </p>
+      </Main>
+    </>
+  );
+}
---
+++ src/components/Test.js
@@ -0,0 +1,15 @@
+export default function Test(props) {
+  return <div></div>;
+}
+
+//Exemplo de utilização de props
+
+/* <Test
+  number={10}
+  string="Teste"
+  visible
+  data={{ a: 1, b: 2 }}
+  onClick={() => {
+    console.log("Click");
+  }}
+/>; */
